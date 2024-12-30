e069056f978e4866d38eaf7f5ef7845afdb1d5e4
Psuedoo <forthewin720@gmail.com>
2024-12-30 11:56:15 -0500

    make it work

--- tests/test_scores.py
+++ tests/test_scores.py
@@ -5,15 +5,16 @@ import pytest
 @pytest.mark.parametrize(
     "test_input,expected",
     [
-        # (1, 100),
-        # (2, 168),
-        # (3, 168),
-        # (4, 122),  # Manually count
+        (1, 100),
+        (2, 168),
+        (3, 168),
+        (4, 122),
         (5, 173),
         (6, 212),
         (7, 127),
         (8, 50),
+        (9, 177),
     ],
 )
 def test_get_score(test_input, expected):
-    assert get_score(f"test-images/test-image-{test_input}.png") == expected
+    assert get_score(f"test-images/{test_input}.png") == expected
--- utils/image.py
+++ utils/image.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 def resize_image(filepath):
+    # This resizes the image but maintains the aspect ratio
     image = cv2.imread(filepath)
 
     (h, w) = image.shape[:2]
@@ -15,31 +16,38 @@ def resize_image(filepath):
     return cv2.resize(image, (new_width, new_height))
 
 
+def get_yellow_mask(image):
+    # Convert colorspace
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Find the yellow and create a mask
+    yellow_lower = np.array([10, 100, 100])
+    yellow_upper = np.array([60, 255, 255])
+
+    mask = cv2.inRange(hsv, yellow_lower, yellow_upper)
+
+    return mask
+
+
+def mask_yellow(image, mask):
+    yellow_ratio = (cv2.countNonZero(mask)) / (image.size / 3)
+
+    humanize_ratio = np.round(yellow_ratio * 100, 2)
+
+    # Don't mask if the background is yellow. This causes a lot of issues
+    if humanize_ratio < 5:
+        mask_invert = cv2.bitwise_not(mask)
+        image = cv2.bitwise_and(image, image, mask=mask_invert)
+
+    return image
+
+
 def filter_image(image):
 
-    # image = np.array(255 * (image / 255) ** 3.2, dtype="uint8")
-    # image = cv2.GaussianBlur(image, (3, 3), cv2.BORDER_DEFAULT)
-    # alpha = 1.5
-    # beta = -200
-    # image = cv2.convertScaleAbs(image, beta=beta)
-    # cv2.imshow("after abs", image)
+    yellow_mask = get_yellow_mask(image)
+    image = mask_yellow(image, yellow_mask)
 
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    ret, thresh = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    # ret, thresh = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    kernel = np.ones((3, 3), np.uint8)
-    closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
-
-    # Background area using Dilation
-    bg = cv2.dilate(closing, kernel, iterations=1)
-
-    # Finding foreground area
-    dist_transform = cv2.distanceTransform(closing, cv2.DIST_L2, 0)
-    ret, fg = cv2.threshold(dist_transform, 0.01 * dist_transform.max(), 255, 0)
-
-    return bg
+    return image
 
 
 def get_params():
@@ -49,7 +57,6 @@ def get_params():
 
     # Set Area filtering parameters
     params.filterByArea = True
-    # params.minArea = 90
     params.minArea = 60
 
     # Set Circularity filtering parameters
@@ -67,30 +74,49 @@ def get_params():
     return params
 
 
-def get_score(filepath):
-    image = resize_image(filepath)
-    image = filter_image(image)
-
+def find_circles(image):
     params = get_params()
 
     # Create a detector with the parameters
     detector = cv2.SimpleBlobDetector_create(params)
 
-    # Detect blobs
     keypoints = detector.detect(image)
 
-    # Draw blobs on our image as red circles
+    return keypoints
+
+
+def circle_domino_dots(image, keypoints):
     blank = np.zeros((1, 1))
-    blobs = cv2.drawKeypoints(
+    circles = cv2.drawKeypoints(
         image, keypoints, blank, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
     )
+    return circles
 
-    number_of_blobs = len(keypoints)
 
-    text = "Number of Circular Blobs: " + str(len(keypoints))
-    cv2.putText(blobs, text, (20, 550), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 100, 255), 2)
-    cv2.imshow("Filtering Circular Blobs Only", blobs)
+def add_text_and_circles_to_image(image, circles, text):
+    cv2.putText(circles, text, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 100, 255), 2)
+
+
+def display(circles):
+    cv2.imshow("Filtering Score", circles)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    return number_of_blobs
+
+def get_score(filepath):
+    image = resize_image(filepath)
+    image = filter_image(image)
+
+    keypoints = find_circles(image)
+
+    circles = circle_domino_dots(image, keypoints)
+    number_of_circles = len(keypoints)
+
+    # Uncomment these lines out to display the image(s) while testing
+
+    # add_text_and_circles_to_image(
+    #     image, circles, text=f"Domino Score: {str(number_of_circles)}"
+    # )
+    # display(circles)
+
+    return number_of_circles
